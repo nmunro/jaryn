074e00f11cf53aac8562ddbf5ce1fa9bb0ab0375
Neil Munro <neilmunro@gmail.com>
2016-02-15 14:04:24 -0500

    Added basic localStorage functions.

---
+++ app.js
@@ -0,0 +1,72 @@
+const Jaryn = function() { return(this === window) ? new Jaryn() : this; };
+
+Jaryn.prototype.UI = {
+  "addRecord": (record) => {
+    const tb = document.getElementById('tb');
+    const tr = document.createElement('tr');
+    const id = document.createElement('td');
+    const date = document.createElement('td');
+    const data = document.createElement('td');
+    
+    id.innerHTML = record.id;
+    date.innerHTML = record.date;
+    data.innerHTML = "Nothing yet!";
+    
+    tr.appendChild(id);
+    tr.appendChild(date);
+    tr.appendChild(data);
+    tb.appendChild(tr);
+  },
+  
+  "removeRecord": (record) => console.log("Unimplemented!"),
+  
+  "removeAllRecords": () => {
+    const tb = document.getElementById('tb');
+    while(tb.hasChildNodes()) tb.removeChild(tb.firstChild);
+  },
+  
+  "setMood": () => {
+    document.getElementById("moodValue").innerHTML = document.getElementById("mood").value + "0%";
+  },
+  
+  "submitStatus": () => {
+    const feelings = Array.from(document.getElementsByClassName("feeling")).filter((element) => element.checked);
+    const mood = document.getElementById("mood").value;
+    console.log({"mood": mood, "feelings": feelings});
+  }
+};
+
+Jaryn.prototype.DB = {
+  "loadRecord": (id) => JSON.parse(localStorage.getItem(id)),
+  
+  "loadRecords": () => Jaryn.prototype.DB.getAll().forEach(Jaryn.prototype.UI.addRecord),
+  
+  "deleteRecord": (id) => localStorage.removeItem(id),
+  
+  "deleteAll": () => {
+    localStorage.clear();
+    Jaryn.prototype.UI.removeAllRecords();
+  },
+  
+  "getAll": () => {
+    const ids = JSON.parse(localStorage.getItem("indexTable")) || [];
+    return ids.map((id) => JSON.parse(localStorage.getItem(id)));
+  },
+  
+  "save": (record) => {
+    const date = new Date();
+    const indexTable = JSON.parse(localStorage.getItem("indexTable")) || [];
+    
+    record.id = date.getTime();
+    record.date = date.toDateString();
+    indexTable.push(date.getTime());
+    localStorage.setItem(date.getTime(), JSON.stringify(record));
+    localStorage.setItem("indexTable", JSON.stringify(indexTable));
+    Jaryn.prototype.UI.addRecord(record);
+    
+    return date.getTime();
+  },
+  
+  "toJSON": () => console.log("Unimplemented!"),
+  "sync": () => console.log("Unimplemented!") 
+};
