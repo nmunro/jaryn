49a58fb37cda00217f166b0e161e93db9bf86015
Neil Munro <neilmunro@gmail.com>
2016-02-22 01:44:38 +0000

    Collection of work on the right navigation pane.

--- app.js
+++ app.js
@@ -2,14 +2,19 @@
   /*const jaryn = new Jaryn();
   jaryn.UI.init();*/
   
+  const showNav = (nav) => nav.setAttribute("class", "navLink active");
+  const hideNav = (nav) => nav.setAttribute("class", "navLink");
   const showDiv = (div) => div.setAttribute("class", "contentDiv col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main");  
   const hideDiv = (div) => div.setAttribute("class", "contentDiv col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main invisible");  
   const toggleDiv = (e) => {
     Array.from(document.getElementsByClassName("contentDiv")).forEach(hideDiv);
     showDiv(document.getElementById(e.target.getAttribute("data-target")));
   };
+  const toggleNav = (e) => {
+    Array.from(document.getElementsByClassName("navLink")).forEach(hideNav);
+    showNav(document.getElementById(e.target));
+  };
       
-  document.querySelector("#dashboard").addEventListener("click", toggleDiv);
-  document.querySelector("#history").addEventListener("click", toggleDiv);
-  document.querySelector("#help").addEventListener("click", toggleDiv);
+  Array.from(document.getElementsByClassName("contentDiv")).forEach((div) => div.addEventListener("click", toggleDiv));
+  Array.from(document.getElementsByClassName("navLink")).forEach((nav) => nav.addEventListener("click", toggleNav));
 }());
