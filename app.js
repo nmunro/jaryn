525f368fe58be98fa1cb3612ba456bb8be9284c3
Neil Munro <neilmunro@gmail.com>
2016-02-22 00:50:39 +0000

    Added jQuery and started gotten some DOM show/hides working.

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 (function() {
-  const jaryn = new Jaryn();
-  jaryn.UI.init();
+  /*const jaryn = new Jaryn();
+  jaryn.UI.init();*/
+  
+  const showDiv = (div) => div.setAttribute("class", "contentDiv col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main");  
+  const hideDiv = (div) => div.setAttribute("class", "contentDiv col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main invisible");  
+  const toggleDiv = (e) => {
+    Array.from(document.getElementsByClassName("contentDiv")).forEach(hideDiv);
+    showDiv(document.getElementById(e.target.getAttribute("data-target")));
+  };
       
-  document.getElementById("mood").addEventListener("change", jaryn.UI.setMood);
-  document.getElementById("submitStatus").addEventListener("click", jaryn.UI.submitStatus);
+  document.querySelector("#dashboard").addEventListener("click", toggleDiv);
+  document.querySelector("#history").addEventListener("click", toggleDiv);
+  document.querySelector("#help").addEventListener("click", toggleDiv);
 }());
