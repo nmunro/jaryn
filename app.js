5730e5b1ee883bd7f42a0534da92feb908d403fb
Neil Munro <neilmunro@gmail.com>
2016-02-22 21:47:47 +0000

    Gotten the basic UI navigation elements working now.

--- app.js
+++ app.js
@@ -6,14 +6,21 @@
   const hideNav = (nav) => nav.setAttribute("class", "navLink");
   const showDiv = (div) => div.setAttribute("class", "contentDiv col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main");  
   const hideDiv = (div) => div.setAttribute("class", "contentDiv col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main invisible");  
-      
-  Array.from(document.getElementsByClassName("contentDiv")).forEach((div) => div.addEventListener("click", (e) => {
+  const toggleDiv = (div) => {
     Array.from(document.getElementsByClassName("contentDiv")).forEach(hideDiv);
-    showDiv(document.getElementById(e.target.getAttribute("data-target")));
-  }));
+    showDiv(div);
+  };   
+  
+  Array.from(document.getElementsByClassName("contentDiv")).forEach((div) => {
+    div.addEventListener("click", (e) => toggleDiv(div));
+  });
 
-  Array.from(document.getElementsByClassName("navLink")).forEach((nav) => nav.addEventListener("click", (e) => {
-    Array.from(document.getElementsByClassName("navLink")).forEach(hideNav);
-    showNav(document.getElementById(e.target));
-  }));
+  Array.from(document.getElementsByClassName("navLink")).forEach((nav) => {
+    nav.addEventListener("click", (e) => {
+      Array.from(document.getElementsByClassName("navLink")).forEach(hideNav);
+      Array.from(document.getElementsByClassName("contentDiv")).forEach(hideDiv);
+      showNav(e.target.parentNode);
+      showDiv(document.getElementById(nav.firstChild.getAttribute("data-target")));
+    });
+  });
 }());
