6c9b7ee736f9abce3a70b75d616f533ddae1bfa2
Neil Munro <neilmunro@gmail.com>
2016-02-23 23:36:08 +0000

    Added basic historical data table.

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 (function() {
-  const jaryn = new Jaryn();
-  jaryn.init();
-  
+  const jaryn = Jaryn();
+  const tbody = document.getElementById("historyTable");  
   const showNav = (nav) => nav.setAttribute("class", "navLink active");
   const hideNav = (nav) => nav.setAttribute("class", "navLink");
   const showDiv = (div) => div.setAttribute("class", "contentDiv col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main");  
@@ -17,10 +16,42 @@
 
   Array.from(document.getElementsByClassName("navLink")).forEach((nav) => {
     nav.addEventListener("click", (e) => {
-      Array.from(document.getElementsByClassName("navLink")).forEach(hideNav);
       Array.from(document.getElementsByClassName("contentDiv")).forEach(hideDiv);
-      showNav(e.target.parentNode);
+      Array.from(document.getElementsByClassName("navLink")).forEach(hideNav);
       showDiv(document.getElementById(nav.firstChild.getAttribute("data-target")));
+      showNav(e.target.parentNode);
     });
+  });
+  
+  jaryn.init();
+  jaryn.getHistory().forEach((obj) => {
+    const row = document.createElement('tr');
+    const id = document.createElement('td');
+    const date = document.createElement('td');
+    const mood = document.createElement('td');
+    const feelings = document.createElement('td');
+    const notes = document.createElement('td');
+    
+    id.innerHTML = obj.id;
+    if((obj.date.getMonth()+1) < 10) 
+      date.innerHTML = obj.date.getFullYear() + "/0" + (obj.date.getMonth() +1) + "/" + obj.date.getDate();
+    else 
+      date.innerHTML = obj.date.getFullYear() + "/" + (obj.date.getMonth() +1) + "/" + obj.date.getDate();
+    
+    if(obj.date.getDate() < 10) 
+      date.innerHTML = obj.date.getFullYear() + "/0" + (obj.date.getMonth() +1) + "/0" + obj.date.getDate();
+    else
+      date.innerHTML = obj.date.getFullYear() + "/" + (obj.date.getMonth() +1) + "/" + obj.date.getDate();
+      
+    mood.innerHTML = obj.mood;
+    feelings.innerHTML = obj.feelings.reduce((prev, current) => prev + ", " + current);
+    notes.innerHTML = obj.notes;
+    
+    row.appendChild(id);
+    row.appendChild(date);
+    row.appendChild(mood);
+    row.appendChild(feelings);
+    row.appendChild(notes);
+    tbody.appendChild(row);
   });
 }());
--- jaryn.js
+++ jaryn.js
@@ -6,10 +6,33 @@ Jaryn.prototype.init = function() {
   console.log("Jaryn: Init");  
 };
 
-Jaryn.prototype.getData = function() {
-  console.log("Jaryn: getData");  
+// This only returns dummy data right now.
+Jaryn.prototype.getHistory = function() {
+  const today = new Date();
+  var yesterday = new Date();
+  yesterday.setDate(today.getDate() -1); 
+  
+  return Object.freeze([{
+    "id": today.getTime(),
+    "date": today,
+    "mood": 5,
+    "feelings": [
+      "ok/fine"
+    ],
+    "notes": "Lorim ipsom"
+  },
+  {
+    "id": yesterday.getTime(),
+    "date": yesterday,
+    "mood": 5,
+    "feelings": [
+      "ok/fine",
+      "tired"
+    ],
+    "notes": "Lorim ipsom"
+  }]);
 };
 
-Jaryn.prototype.setData = function() {
+Jaryn.prototype.setData = function(data) {
   console.log("Jaryn: setData");  
 };
