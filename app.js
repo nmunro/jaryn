70ecaf6eac212a3842e4a6bb60a1142d5b0fea04
Neil Munro <neilmunro@gmail.com>
2016-02-29 22:54:11 +0000

    Added date and moved to querySelector* methods.

--- app.js
+++ app.js
@@ -5,27 +5,43 @@
   const showDiv = (div) => div.setAttribute("class", "contentDiv col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main");
   const hideDiv = (div) => div.setAttribute("class", "contentDiv col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main invisible");
   const toggleDiv = (div) => {
-    Array.from(document.getElementsByClassName("contentDiv")).forEach(hideDiv);
+    Array.from(document.querySelectorAll(".contentDiv")).forEach(hideDiv);
     showDiv(div);
   };
+  const setDate = (() => {
+    const now = new Date();
+    const date = document.querySelector("#date");
+    var month = now.getMonth()+1;
+    var day = now.getDate();
+    
+    if(month < 10) month = "" + 0 + month;
+    if(day < 10) day = "" + 0 + day;
+      
+    date.innerHTML = day + "/" + month + "/" + now.getFullYear();
+  });
+  
+  setDate();
 
-  Array.from(document.getElementsByClassName("contentDiv")).forEach((div) => {
+  Array.from(document.querySelectorAll(".contentDiv")).forEach((div) => {
     div.addEventListener("click", (e) => toggleDiv(div));
   });
 
   Array.from(document.getElementsByClassName("navLink")).forEach((nav) => {
     nav.addEventListener("click", (e) => {
-      Array.from(document.getElementsByClassName("contentDiv")).forEach(hideDiv);
-      Array.from(document.getElementsByClassName("navLink")).forEach(hideNav);
-      showDiv(document.getElementById(nav.firstChild.getAttribute("data-target")));
+      Array.from(document.querySelectorAll(".contentDiv")).forEach(hideDiv);
+      Array.from(document.querySelectorAll(".navLink")).forEach(hideNav);
+      showDiv(document.querySelector("#" + nav.firstChild.getAttribute("data-target")));
       showNav(e.target.parentNode);
     });
   });
 
+  // Populate history table.
   jaryn.io.readHistory((data) => {
-    const tbody = document.getElementById("historyTable");
-    const averageMood = document.getElementById("averageMood");
-    var average = 0;
+    const tbody = document.querySelector("#historyTable");
+    const moods = document.querySelector("#averageMood");
+    const emotions = document.querySelector("#averageEmotions");
+    var averageMood = 0;
+    var averageEmotions = [];
     
     data.forEach((obj) => {
       const row = document.createElement('tr');
@@ -42,7 +58,8 @@
       id.innerHTML = obj.id;
       date.innerHTML = day + "/" + month + "/" + year;
       mood.innerHTML = obj.mood;
-      average += obj.mood;
+      averageMood += obj.mood;
+      averageEmotions.push(obj.feelings);
       feelings.innerHTML = obj.feelings.reduce((prev, current) => prev + ", " + current);
       notes.innerHTML = obj.notes;
 
@@ -53,7 +70,9 @@
       row.appendChild(notes);
       tbody.appendChild(row);
     });
-    average /= data.length;
-    averageMood.innerHTML = average;
+    
+    averageMood /= data.length;
+    moods.innerHTML = averageMood;
+    emotions.innerHTML = averageEmotions.join(", ");
   });
 }());
