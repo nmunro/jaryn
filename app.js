95cd258d463dd817657943978c4140a920cb0003
Neil Munro <neilmunro@gmail.com>
2016-03-26 00:22:03 +0000

    [#9] Gotten the UI in place now, event handlers need to be implemented.

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ const App = Object.freeze(Object.create({
   },
   
   "setMoodValue": function(value) {
-    document.querySelector('#moodValue').innerHTML = value;
+    document.querySelector('#addMoodValue').innerHTML = value;
   },
   
   "setSevenDayAverageMood": function(val) {
@@ -48,9 +48,9 @@ const App = Object.freeze(Object.create({
   "setupEventHandlers": function() {
      // Save button event handler.
     document.querySelector("#save").addEventListener("click", () => {
-      const mood = document.querySelector("#mood").value;
-      const notes = document.querySelector("#notes").value;
-      const nodes = document.querySelectorAll(".emotion");
+      const mood = document.querySelector("#addMood").value;
+      const notes = document.querySelector("#addNotes").value;
+      const nodes = document.querySelector("#addDiv").querySelectorAll(".emotion");
       const feelings = Array.from(nodes).filter((node) => node.checked);
       const now = DateUtil.getDate();
       const data = {};
@@ -59,17 +59,26 @@ const App = Object.freeze(Object.create({
       data.date = now;
       data.mood = parseInt(mood, 10);
       data.notes = notes;
-      data.feelings = feelings.map((node) => node.id); 
+      data.feelings = feelings.map((node) => node.getAttribute("data-name")); 
       
+      // Have to use a function in this scope.
       VFS.updateDiary(data, () => this.init());
+    });
+    
+    document.querySelector("#resave").addEventListener("click", () => {
+      // Override object.
+      // Return to dashboard.
+      console.log("Re-saving!");  
+    });
+    
+    document.querySelector("#cancel").addEventListener("click", () => {
+      // Clear inut controls.
+      // Return to dashboard.
+      console.log("Cancelling!");  
     });
 
     // Add event handlers.
-    Array.from(document.querySelectorAll(".contentDiv")).forEach((div) => {
-      div.addEventListener("click", () => this.toggleDiv(div));
-    });
-  
-    document.querySelector("#mood").addEventListener('change', (event) => {
+    document.querySelector("#addMood").addEventListener('change', (event) => {
       this.setMoodValue(event.target.value);
     });
 
@@ -79,14 +88,45 @@ const App = Object.freeze(Object.create({
         
         this.hideDivAll();
         this.hideNavAll();
-        this.showDiv(document.querySelector("#" + target));
+        this.showDiv(document.querySelector(`#${target}`));
         this.showNav(event.target.parentNode);
       });
     }); 
   },
   
   "editHistory": function(event) {
-    console.log(event.target.parentNode);  
+    const row = event.target.parentNode;
+    const cells = Array.from(row.getElementsByTagName("td"));
+    const es = Array.from(document.querySelector("#editDiv").querySelectorAll(".emotion"));
+    
+    this.hideDivAll();
+    this.showDiv(document.querySelector("#editDiv"));
+    
+    cells.forEach((cell) => {
+      switch(cell.getAttribute("data-name")) {
+        case "date":
+          document.getElementById("editDate").innerHTML = cell.innerHTML;
+          break;   
+          
+        case "mood": 
+          document.getElementById("editMoodValue").innerHTML = cell.innerHTML;
+          document.getElementById("editMood").value = cell.innerHTML[0];
+          break;
+          
+        case "feelings":
+          cell.innerHTML.split(" ").forEach((text) => {
+            if(text.endsWith(",")) text = text.slice(0, -1);
+            es.forEach((e) => {
+              if(e.getAttribute("data-name") === text) e.checked = true;  
+            });  
+          });
+          break;
+          
+        case "notes":
+          document.getElementById("editNotes").innerHTML = cell.innerHTML;
+          break;
+      }
+    });
   },
   
   "showHistory": function(data) {
@@ -120,6 +160,11 @@ const App = Object.freeze(Object.create({
       date.innerHTML = `${dt.getDate()}/`;
       date.innerHTML += `${DateUtil.zeroPad(dt.getMonth()+1)}/`;
       date.innerHTML += `${dt.getFullYear()}`;
+      
+      date.setAttribute("data-name", "date");
+      mood.setAttribute("data-name", "mood");
+      feelings.setAttribute("data-name", "feelings");
+      notes.setAttribute("data-name", "notes");
 
       row.appendChild(date);
       row.appendChild(mood);
@@ -128,7 +173,7 @@ const App = Object.freeze(Object.create({
       tbody.appendChild(row);
       
       // Add event handler for the row.
-      row.addEventListener("click", this.editHistory);
+      row.addEventListener("click", (event) => this.editHistory(event));
     });
     
     // Bail out if there's no data.
