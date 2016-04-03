875f5acaf27b36bb1cb6eac7bc00a57d48b6f01c
Neil Munro <neilmunro@gmail.com>
2016-04-03 22:39:42 +0100

    [#30] Split Dashboard into 7 day history and 7 day charts.

--- app.js
+++ app.js
@@ -2,39 +2,39 @@ const App = Object.freeze(Object.create({
   "showNav": function(nav) {
     nav.classList.add("active");
   },
-  
+
   "hideNav": function(nav) {
     nav.classList.remove("active");
   },
-  
+
   "hideNavAll": function() {
     Array.from(document.querySelectorAll(".navLink")).forEach(this.hideNav);
   },
-  
+
   "showDiv": function(div) {
     div.classList.remove("invisible");
   },
-  
+
   "hideDiv": function(div) {
     div.classList.add("invisible");
   },
-  
+
   "hideDivAll": function() {
     Array.from(document.querySelectorAll(".contentDiv")).forEach(this.hideDiv);
   },
-  
+
   "toggleDiv": function(div) {
     this.hideDivAll();
     this.showDiv(div);
   },
-  
+
   "showDefaultDashboard": function() {
     this.hideDivAll();
     this.hideNavAll();
     this.showDiv(document.querySelector("#dashboardDiv"));
     this.showNav(document.querySelector("#dashboardNav").parentNode);
   },
-  
+
   "setDate": function() {
     const text = [6, 0].map((n) => {
       const {year, month, day} = DateUtil.getYYYYMMDD(n);
@@ -42,51 +42,59 @@ const App = Object.freeze(Object.create({
     });
     document.querySelector("#date").innerHTML = text.join("&nbsp;-&nbsp;");
   },
-  
+
   "setMoodValue": function(value) {
     document.querySelector('#moodValue').innerHTML = value;
   },
-  
+
   "setSevenDayAverageMood": function(val) {
-    document.querySelector("#moodMeter").value = val*10;  
+    document.querySelector("#moodMeter").value = val*10;
     document.querySelector("#moodMeterLbl").innerHTML = `${Math.floor(val*10)}%`;
   },
-  
+
   "setSevenDayAverageEmotions": function(emotions) {
     const obj = {};
     emotions.forEach((day, c1, a1) => {
       day.forEach((emotion, c2, a2) => {
         if(obj[emotion] === undefined) obj[emotion] = { "count": 1, "percent": 0 };
         else obj[emotion].count++;
-        
+
         // This is the end of both inner and outer loops.
         if(c1 === a1.length-1 && c2 === a2.length-1) {
           const total = Object.keys(obj).map((elm) => obj[elm]).reduce((p, n) => p + n.count, 0);
           const emotions = document.querySelector("#averageEmotions");
           const exercise = document.querySelector("#exercise");
-          
+
           emotions.innerHTML = "";
           Object.keys(obj).map((emotion) => {
             const percent = Math.round((obj[emotion].count / total) * 100);
             obj[emotion].percent = percent;
-            emotions.innerHTML += `${emotion}: ${percent}%&nbsp;|&nbsp;`;  
+            emotions.innerHTML += `${emotion}: ${percent}%&nbsp;|&nbsp;`;
           });
         }
       });
     });
   },
-  
+
   "clearHistoryEntry": function() {
     const {year, month, day} = DateUtil.getYYYYMMDD();
-    Array.from(document.querySelectorAll(".emotion")).forEach((e) => e.checked = false);    
+    Array.from(document.querySelectorAll(".emotion")).forEach((e) => e.checked = false);
     document.querySelector("#exercise").checked = false;
     document.querySelector("#notes").innerHTML = "";
     document.querySelector("#mood").value = "0";
     document.querySelector("#moodValue").innerHTML = "0%";
     document.querySelector("#editDate").innerHTML = `${day}/${month}/${year}`;
   },
-  
+
   "setupEventHandlers": function() {
+    $('.tab-nav a').click(function (e) {
+      console.log(this);
+      e.preventDefault()
+      $(this).tab('show');
+    });
+
+    $('.tab-nav a:first').tab('show');
+
      // Save button event handler.
     document.querySelector("#saveRecord").addEventListener("click", () => {
       const nodes = document.querySelectorAll(".emotion");
@@ -98,14 +106,14 @@ const App = Object.freeze(Object.create({
         "exercise": document.querySelector("#exercise").checked,
         "feelings": feelings.map((node) => node.getAttribute("data-name"))
       };
-      
+
       // Have to use a function in this scope.
       VFS.updateDiary(data, () => {
         this.init();
         this.clearHistoryEntry();
       });
     });
-    
+
     document.querySelector("#cancelRecord").addEventListener("click", () => {
       this.clearHistoryEntry();
       this.showDefaultDashboard();
@@ -119,65 +127,65 @@ const App = Object.freeze(Object.create({
     Array.from(document.querySelectorAll(".navLink")).forEach((nav) => {
       nav.addEventListener("click", (event) => {
         const target = nav.firstChild.getAttribute("data-target");
-        
+
         this.hideDivAll();
         this.hideNavAll();
         this.showDiv(document.querySelector(`#${target}`));
         this.showNav(event.target.parentNode);
         this.clearHistoryEntry();
       });
-    }); 
+    });
   },
-  
+
   "editHistory": function(event) {
     const row = event.target.parentNode;
     const cells = Array.from(row.querySelectorAll("td"));
     const es = Array.from(document.querySelectorAll(".emotion"));
-    
+
     this.hideDivAll();
     this.hideNavAll();
     this.showDiv(document.querySelector("#editDiv"));
     this.showNav(document.querySelector("#editNav").parentNode);
-    
+
     cells.forEach((cell) => {
       switch(cell.getAttribute("data-name")) {
         case "date":
           document.querySelector("#editDate").innerHTML = cell.innerHTML;
-          break;   
-          
-        case "mood": 
+          break;
+
+        case "mood":
           document.querySelector("#moodValue").innerHTML = cell.innerHTML;
           document.querySelector("#mood").value = cell.innerHTML[0];
           if(cell.innerHTML === "100%") document.querySelector("#mood").value += "0";
           break;
-          
+
         case "feelings":
           cell.innerHTML.split(" ").forEach((text) => {
             if(text.endsWith(",")) text = text.slice(0, -1);
             es.forEach((e) => {
-              if(e.getAttribute("data-name") === text) e.checked = true;  
-            });  
+              if(e.getAttribute("data-name") === text) e.checked = true;
+            });
           });
           break;
-          
+
         case "notes":
           document.querySelector("#notes").innerHTML = cell.innerHTML;
           break;
-          
+
         case "exercise":
           document.querySelector("#exercise").checked = cell.innerHTML === "Yes";
           break;
       }
     });
   },
-  
+
   "showHistory": function(data) {
     const moodObj = [];
     const emotionObj = [];
     const tbody = document.querySelector("#historyTable");
-    
+
     while(tbody.hasChildNodes()) tbody.removeChild(tbody.firstChild);
-    
+
     data.forEach((obj) => {
       const row = document.createElement('tr');
       const date = document.createElement('td');
@@ -187,16 +195,16 @@ const App = Object.freeze(Object.create({
       const exercise = document.createElement('td');
       const [year, month, day] = obj.date.split("-");
       const fn = (p, n) => `${p}, ${n}`;
-      
+
       moodObj.push(obj.mood || 0);
       emotionObj.push(obj.feelings);
-      
+
       mood.innerHTML = `${obj.mood*10 || 0}%`;
       feelings.innerHTML = obj.feelings.length > 0 ? obj.feelings.reduce(fn) : "-";
       notes.innerHTML = (obj.notes !== "") ? obj.notes : "-";
       date.innerHTML = `${day}/${month}/${year}`;
       exercise.innerHTML = obj.exercise ? "Yes" : "No";
-      
+
       date.setAttribute("data-name", "date");
       mood.setAttribute("data-name", "mood");
       feelings.setAttribute("data-name", "feelings");
@@ -209,17 +217,17 @@ const App = Object.freeze(Object.create({
       row.appendChild(notes);
       row.appendChild(exercise);
       tbody.appendChild(row);
-      
+
       // Add event handler for the row.
       row.addEventListener("click", (event) => this.editHistory(event));
     });
-    
+
     // Bail out if there's no data.
     if(moodObj.length === 0) return;
     this.setSevenDayAverageMood(moodObj.reduce((p, n) => p+n) / moodObj.length);
     this.setSevenDayAverageEmotions(emotionObj);
   },
-  
+
   "init": function() {
     this.setDate();
     this.setMoodValue("5");
@@ -227,9 +235,9 @@ const App = Object.freeze(Object.create({
     this.hideNavAll();
     this.showDiv(document.querySelector("#dashboardDiv"));
     this.showNav(document.querySelector("#dashboardNav").parentNode);
-    
+
     VFS.loadConfig((conf) => VFS.getSevenDayHistory((d) => this.showHistory(d)));
-  } 
+  }
 }));
 
 App.setupEventHandlers();
--- vfs.js
+++ vfs.js
@@ -1,11 +1,15 @@
 const configFile = "jaryn-config.json";
 
+isEmpty = function(o) {
+  return Object.keys(o).reduce((p, n) => n ? p+1 : p, 0) <= 0;
+};
+
 /**
- * readJSON gets the data file for the given filename and executes the 
+ * readJSON gets the data file for the given filename and executes the
  * given callback with the data stored in the file passed in as an argument.
- * 
+ *
  * readJSON does NOT create a file if it doesn't already exist.
- * 
+ *
  * @param object obj An object containing the file name and success/failure
  * callback functions.
  */
@@ -21,15 +25,15 @@ const readJSON = (obj) => {
             else obj.onSuccess(JSON.parse(fr.result));
           };
           fr.readAsText(file);
-        });  
-      }, obj.onFailure);    
+        });
+      }, obj.onFailure);
   });
 };
 
 /**
  * writeJSON writes data to the specified file, creating it if it does not exist.
  * Ensure data is a JSON object!
- * 
+ *
  * @param object obj An object containing the file name, data and success or
  * failure callback functions.
  */
@@ -47,21 +51,21 @@ const writeJSON = (obj) => {
         obj.onSuccess(obj.data);
       });
     }, obj.onFailure);
-  }); 
+  });
 };
 
 const VFS = Object.freeze(Object.create({
   "readJSON": readJSON,
   "writeJSON": writeJSON,
   /**
-   * loadConfig loads a config object from the config file and passes the 
+   * loadConfig loads a config object from the config file and passes the
    * config into the callback.
-   * 
+   *
    * @param function cb Callback function to execute upon init.
    */
   "loadConfig": function(cb) {
     const config = {"averageMood": 5, "moodCount": 0, "averageEmotion": []};
-    
+
     readJSON({
       "fileName": configFile,
       "permissions": { "create": false },
@@ -69,10 +73,10 @@ const VFS = Object.freeze(Object.create({
       "onFailure": (err) => this.saveConfig(config, cb)
     });
   },
-  
+
   /**
    * saveConfig saves a config object to the config file.
-   * 
+   *
    * @param object config Config object to save.
    * @param function cb Callback to execute when saved.
   */
@@ -85,11 +89,11 @@ const VFS = Object.freeze(Object.create({
       "onFailure": (err) => console.log(err)
     });
   },
-  
+
   /**
    * loadHistory reads the current months history file and passes the data
    * into the provided callback function.
-   * 
+   *
    * @param String fn File name to open.
    * @param function cb Callback to execute once history has been loaded.
    */
@@ -101,39 +105,39 @@ const VFS = Object.freeze(Object.create({
       "onFailure": (err) => cb([])
     });
   },
-  
+
   /**
    * getSevenDayHistory gets the last seven days of historical information.
-   * 
+   *
    * @param function cb The callback function to execute.
    */
    "getSevenDayHistory": function(cb) {
-     const obj = [];  
+     const obj = [];
      const today = DateUtil.getDate();
      const entries = {};
      let offset = 0;
-     
+
      // Get list of files, at most there will only ever be two and the number
      // of entries to read from the file.
     [0, 1, 2, 3, 4, 5, 6].forEach((num) => {
       const now = DateUtil.getYYYYMMDD(num);
       const fn = `${now.year}-${now.month}.json`;
-      
+
       entries[fn] = entries[fn] === undefined ? 1 : entries[fn] += 1;
     });
-    
+
     Object.keys(entries).forEach((entry) => {
       this.loadHistory(entry, (data) => {
         Array(entries[entry]).fill(0).forEach((o, count, arr) => {
           const date = new Date(today.getTime());
           let tmpDate;
           let entry;
-          
+
           date.setDate(date.getDate()-offset);
           tmpDate = DateUtil.getYYYYMMDD(offset);
           offset++;
-          
-          if(data[tmpDate.day] === undefined || Object.isEmpty(data)) {
+
+          if(data[tmpDate.day] === undefined || isEmpty(data)) {
             obj.push({
               "date": `${tmpDate.year}-${tmpDate.month}-${tmpDate.day}`,
               "notes": "-",
@@ -142,9 +146,9 @@ const VFS = Object.freeze(Object.create({
             });
           }
           else {
-            obj.push(data[tmpDate.day]);   
+            obj.push(data[tmpDate.day]);
           }
-          
+
           if(count === arr.length-1) cb(obj);
         });
       });
@@ -152,9 +156,9 @@ const VFS = Object.freeze(Object.create({
    },
 
   /**
-   * updateDiary takes a single entry and saves it to the current months 
+   * updateDiary takes a single entry and saves it to the current months
    * history file.
-   * 
+   *
    * @param Object day The data for the day.
    * @param function cb the Callback function to execute.
    */
@@ -171,7 +175,7 @@ const VFS = Object.freeze(Object.create({
         "onFailure": (err) => console.log(err)
       });
     };
-    
+
     readJSON({
       "fileName": fn,
       "permissions": { "create": false },
