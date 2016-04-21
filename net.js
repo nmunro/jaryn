b4624eafe013cbc029f09488e661f022c5e9f12e
Neil Munro <neilmunro@gmail.com>
2016-04-21 22:24:15 +0100

    [#2] Added function stubs for uploading/downloading data.

--- net.js
+++ net.js
@@ -1,5 +1,13 @@
 const Net = Object.freeze(Object.create({
   "getNetworkStatus": function() {
     return navigator.onLine ? "Online" : "Offline"; 
-  }  
+  },
+  
+  "download": function() {
+    console.log("Downloading data.");  
+  },
+  
+  "upload": function() {
+    console.log("Uploading data.");  
+  }
 }));
